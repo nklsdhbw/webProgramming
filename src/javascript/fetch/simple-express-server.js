94ec0af6338a97f203d4421797df9a9fbd92116f
Niklas <scholz-niklas@gmx.de>
2023-01-16 15:24:56 +0000

    test

--- src/javascript/fetch/simple-express-server.js
+++ src/javascript/fetch/simple-express-server.js
@@ -2,6 +2,12 @@ const app = require('./app');
 
 const port = process.env.PORT || 8080;
 
+/*
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
+*/
+app.listen(8080, 'localhost', () => {
+
+});
+
