2435a978f778807d2629d0f85175b20c1ec42ede
Niklas <nikwh14@gmx-ist-cool.de>
2023-01-17 19:32:41 +0100

    no setup proxy

--- src/javascript/fetch/simple-express-server.js
+++ src/javascript/fetch/simple-express-server.js
@@ -2,12 +2,13 @@ const app = require('./app');
 
 const port = process.env.PORT || 8080;
 
-/*
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
-*/
+
+/*
 app.listen(8080, 'localhost', () => {
 
 });
-
+*/
